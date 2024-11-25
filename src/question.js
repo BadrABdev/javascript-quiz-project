fac09910e1eb8230030c07d9b5b208aad4357906
Badr AB <aitbella.b@gmail.com>
2024-11-25 16:19:03 +0100

    quiz Project test

--- src/question.js
+++ src/question.js
@@ -1,4 +1,14 @@
 class Question {
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+
+
+
+
     // YOUR CODE HERE:
     //
     // 1. constructor (text, choices, answer, difficulty)
